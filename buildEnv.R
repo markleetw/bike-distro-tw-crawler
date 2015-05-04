652eb99900595db7434434458851c971f38816aa
marklee <marklee@intumit.com>
2015-05-04 15:41:35 +0800

    use "buildEnv.R" to install necessary packages

---
+++ buildEnv.R
@@ -0,0 +1,4 @@
+install.packages("shiny", dependencies = TRUE)
+install.packages("devtools", dependencies = TRUE)
+install.packages('ggplot2', dependencies = TRUE)
+devtools::install_github("rstudio/shinyapps")
