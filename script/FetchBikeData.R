f0a4981ca7f67a6290943bf06675af8c84b735ad
marklee <marklee@intumit.com>
2015-05-12 11:50:41 +0800

    add some description and move the buildEnv file

--- script/FetchBikeData.R
+++ script/FetchBikeData.R
@@ -6,7 +6,7 @@ suppressPackageStartupMessages(library(XML))
 suppressPackageStartupMessages(library(plyr))
 
 # If use cmd "Rscript" to call this R file, the working directory will be
-# ".../TaiwanCityBikePrediction/scipt", which is incorrect, so add this to
+# ".../TaiwanCityBikePrediction/script", which is incorrect, so add this to
 # reset working directory.
 setwd(gsub("script", "", getwd()))
 
